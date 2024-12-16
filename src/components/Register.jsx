589c8059599fde1e2a5f101038dfa03e89d22cca
matperez01 <144254480+matperez01@users.noreply.github.com>
2024-12-16 13:27:53 -0300

    se creo el amacenamiento del registro en el localstorage

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,29 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import imagenregister from "../assets/img/imagenregister.webp";
+import { agregarUsuario, obtenerUsuarios } from '../database.js';
 
 
 function Register() {
+
+
+
+  const [formulario, setFormulario ] = useState({ 
+    nombre: "",
+    usuario: "",
+    email:  "",
+    contraseña:  "",
+    confirmarContraseña: "",
+    check: false,
+  });
+
+  
+
+  const handleChange =(e) =>{
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+
+    if(!formulario.email=== "" || !formulario.usuario==="" || !formulario.email==="" || !formulario.contraseña || !formulario.confirmarContraseña ){
+      alert("debe completar los campos obligatorios!")
+    }
+    if(!formulario.check){
+      alert("Debe aceptar los terminos y condiciones!")
+    }
+
+    if(formulario.contraseña.length < 5){
+      alert("la contraseña debe contener mas caracteres")
+    }
+
+    if (formulario.contraseña !== formulario.confirmarContraseña) {
+      alert("Las contraseñas no coinciden.");
+     
+    }
+    
+    agregarUsuario(formulario);
+
+    alert("Usuario registrado correctamente.");
+    console.log(obtenerUsuarios());
+
+    
+
+    
+  }
+
   return (
     <>
-          <section class="container my-5">
+          <section className="container my-5">
             
             
-            <article class="row">
-              <div class="d-none d-lg-block col-lg-6">
+            <article className="row">
+              <div className="d-none d-lg-block col-lg-6">
                 <img
                   src={imagenregister}
                   alt="imagen de un cocinero con muchos cupcake"
-                  class="imgRegistro rounded-4 img-fluid"
+                  className="imgRegistro rounded-4 img-fluid"
                 />
               </div>
     
               <div
-                class="contenedorregistro col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center"
+                className="contenedorregistro col-sm-12 col-md-12 col-lg-6 d-flex justify-content-center"
               >
-                <form class="formregistro px-3 px-md-4 py-3 shadow rounded-2">
+                <form className="formregistro px-3 px-md-4 py-3 shadow rounded-2" onSubmit={handleSubmit}>
 
                 
                 <div className="mb-3">
@@ -34,30 +85,37 @@ function Register() {
                 <label className="form-label">Nombre y Apellido</label>
                 <input
                   type="text"
-                  name="usuario"
+                  name="nombre"
                   className="casilla form-control"
                   placeholder="Enzo Perez"
+                  value={formulario.nombre}
+                  onChange={handleChange}
                 />
               </div>
+              </div>
 
-             
+           
                 <label className="form-label">Usuario</label>
                 <input
                   type="text"
                   name="usuario"
                   className="casilla form-control"
                   placeholder="Ingrese su nombre de usuario"
+                  value={formulario.usuario}
+                  onChange={handleChange}
                 />
-              </div>
+            
 
               <div className="mb-3">
                
                 <label className="form-label">Correo Electronico </label>
                 <input
                   type="text"
-                  name="usuario"
+                  name="email"
                   className="casilla form-control"
                   placeholder="Correo@electronico.com"
+                  value={formulario.email}
+                  onChange={handleChange}
                 />
               </div>
     
@@ -68,6 +126,8 @@ function Register() {
                   name="contraseña"
                   className="casilla form-control"
                   placeholder="Ingrese su contraseña"
+                  value={formulario.contraseña}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -75,11 +135,27 @@ function Register() {
                 <label className="form-label">Confirmar Contraseña</label>
                 <input
                   type="password"
-                  name="contraseña"
+                  name="confirmarContraseña"
                   className="casilla form-control"
                   placeholder="Ingrese nuevamente su contraseña"
+                  value={formulario.confirmarContraseña}
+                  onChange={handleChange}
                 />
               </div>
+
+              <div className="form-check mb-3">
+              <input
+              className="form-check-input"
+              type="checkbox"
+              name="check"
+              value={formulario.check}
+              onChange={handleChange}
+              
+              />
+            <label className="form-check-label text-body-secondary  ">
+              Acepto la Política de Privacidad y los Términos del Servicio
+            </label>
+            </div>
     
               <div className="text-center">
                 <button type="submit" className="btn btn-secondary w-100">
---
+++ src/database.js
@@ -0,0 +1,18 @@
+let usuarios = JSON.parse(localStorage.getItem("userData")) || [];
+
+
+export const agregarUsuario = (usuario) => {
+  usuarios.push(usuario);
+  localStorage.setItem("userData", JSON.stringify(usuarios));
+};
+
+
+export const obtenerUsuarios = () => {
+  return usuarios;
+};
+
+
+export const resetearUsuarios = () => {
+  usuarios = [];
+  localStorage.setItem("userData", JSON.stringify(usuarios));
+};
