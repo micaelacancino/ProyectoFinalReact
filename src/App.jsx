8068efa586e5a10bbe9d28f9a4c93b7bae1416de
Gerardo Solorzano <gerardosolorzanoprog@gmail.com>
2024-12-19 10:11:41 -0300

    arreglee cambios en el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
-import NavBar from './components/NavBar'
+
+import NavBar from "./components/NavBar"
 function App() {
  
 
   return (
     <>
-    <div>
-      <NavBar/>
-    </div>
+    <NavBar/>
     </>
   )
 }
