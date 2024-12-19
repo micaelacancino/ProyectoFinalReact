778cba118682a53735be2a7672f786a745611cfe
Gerardo Solorzano <gerardosolorzanoprog@gmail.com>
2024-12-19 19:02:14 -0300

    guarde cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 
 import Footer from "./components/Footer"
-=======
 //import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./pages/Home/HomePage";
@@ -9,7 +8,7 @@ import { useEffect, useState } from "react";
 import { obtenerCursos } from "./helpers/bdLocal";
 import DetalleCurso from "./pages/Home/detalleCurso/DetalleCurso";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> 331c7eaf425d49e4e67c28a72b1882dbef787b97
+
 
 function App() {
   const [cursosAlmacenados, setCursosAlmacenados] = useState([]);
@@ -45,13 +44,5 @@ function App() {
 
 export default App;
 
-<<<<<<< HEAD
-  return (
-    <>
-    <Footer/>
-    </>
-  )
-}
-=======
->>>>>>> 331c7eaf425d49e4e67c28a72b1882dbef787b97
+
 
