9296c66370c625b645d61cb7756e5e954c90cc16
Mariano <pabloocon@MacBook-Pro-de-Pablo.local>
2024-12-19 23:04:09 -0300

    Pagina de 404 y de info del equipo, con su css respectivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 //import './App.css'
 
+import Error404 from "./components/error404"
+import Nosotros from "./components/team"
+
 function App() {
  
 
   return (
     <>
-    
+    <Nosotros/>
     </>
   )
 }
