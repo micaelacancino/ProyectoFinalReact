7c627be80730afb7b2007934b8ac4cfe42ac68b2
Gerardo Solorzano <gerardosolorzanoprog@gmail.com>
2025-01-07 16:19:19 -0300

    guarde cambios en nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -45,7 +45,7 @@ function Nosotros() {
             <img src="https://scontent.fsla1-1.fna.fbcdn.net/v/t1.6435-1/60259623_10218336369056257_6265658923181670400_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=100&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeEJm7e1_5qGhA585QyNR70p0TfM1Uu21HDRN8zVS7bUcNBgbpX1qYC71vGuq7xJ-HU&_nc_ohc=chrwqdG85SMQ7kNvgEIMAQI&_nc_oc=AdgUNKn9Tf_t8uknYn0GCtDTYKPLG9NFfXsj4BK9CcC-wTuiHhnLcOXnLhmfkkE3bSfIhd5_Os9_jKGkumd37Ujl&_nc_zt=24&_nc_ht=scontent.fsla1-1.fna&_nc_gid=AlQpl69UfxOFcQBUbWygNYe&oh=00_AYAQIlUu0biSDgV5yicX1FhU54ooL8TzayP1cf6z3lvAww&oe=678B0ED0" alt="Nombre 4" className="member-photo" />
           </div>
           <div className="info">
-            <h2 className="name">Gerardo Salorzano</h2>
+            <h2 className="name">Gerardo Solorzano</h2>
             <p className="description">Músico, violinista de la orquesta sinfónica de Salta. Me encanta el arte y siento que el universo de la programación es un arte de creatividad infinita.</p>
             <i className="bi bi-github " style={{color:"white"}}>  <a href="https://github.com/gerardosolorzano-33" className="role"> Perfil de Github</a> </i>
           </div>
